dd50da5e8f3d528c2c9ff7a74308ae7eff62c036
eoagyen <obengagyen@gmail.com>
2025-11-01 16:33:23 +0000

    Added practice files for adding and removing elements in list

---
+++ python_work/try_it_yourself/lists/names_list.py
@@ -0,0 +1,7 @@
+friends_names = ['Gloria', 'Israel', 'Christabel', 'Emmanuel', 'Samuel']
+
+print(f"Hello {friends_names[0]}, I am glad to have you as my friend.")
+print(f"Hello {friends_names[1]}, I am glad to have you as my friend.")
+print(f"Hello {friends_names[2]}, I am glad to have you as my friend.")
+print(f"Hello {friends_names[3]}, I am glad to have you as my friend.")
+print(f"Hello {friends_names[4]}, I am glad to have you as my friend.")
